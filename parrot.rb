d9083e2f7f360650f77ab30fd72ac671824148f3
Scott  Keplinger <skeplin17@gmail.com>
2020-05-04 21:35:06 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,3 +1,6 @@
+# Create method `parrot` that outputs a given phrase and
+# returns the phrase
 def parrot(phrase="Squak!")
-  return #{phrase}
+  puts #{"Pretty bird!"}
+  return "Pretty bird!"
 end
